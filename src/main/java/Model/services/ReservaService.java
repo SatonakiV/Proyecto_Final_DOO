4021ad98efccd8bae02137c55aa7d49d1bc6f5f2
Gerhec <gerhecp@gmail.com>
2026-06-19 18:15:04 -0400

    servicio base de reservas para validacion de cupos
    version 1.0
    
    agrega una estructura inicial del servicio encargado de contar los estudiantes inscritos. se agregó como soporte basico para que la busqueda por cupos pueda hacer el calculo.

---
+++ src/main/java/Model/services/ReservaService.java
@@ -0,0 +1,17 @@
+package Model.services;
+
+import Model.entidades.BloqueHorario;
+
+public class ReservaService {
+
+    public ReservaService() {
+    }
+
+
+    public int contarEstudiantesEnBloque(String tutorId, String materiaNombre, BloqueHorario bloque) {
+        return 0;
+    }
+}
+
+
+// reserva version 1.0 para prueba de codigo cupo
